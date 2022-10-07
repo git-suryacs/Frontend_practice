85a57f3c6e1d2ab1909a271c300bad6a88500431
git-suryacs <2suryacs@gmail.com>
2022-10-07 10:29:09 +0100

    javascript introduction

---
+++ Javascript/example.js
@@ -0,0 +1,20 @@
+var x = 10; // global scope
+console.log(x);
+/* data types:
+undefined
+null
+boolean
+string
+symbol
+number
+object
+*/
+
+
+let r = "Hey there!" // local scope
+
+const pi = 3.14; // constant no change
+console.log(pi*2);
+
+var a; 
+console.log(a)
